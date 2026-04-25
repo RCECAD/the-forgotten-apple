d53fbf6dbc7e8297e6399ed3dd6eb680e5424681
giulliaVilanova <govilanova@gmail.com>
2026-04-25 00:58:57 -0300

    feat: added player movements

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -3,10 +3,12 @@ extends CharacterBody2D
 
 const GRAVITY = 1000
 const SPEED = 300
+const JUMP_FORCE = -400
 
-enum State {Idle, Run}
+enum State {Idle, Run, Jump, Fall, Get_Down, Stay_Down, Get_Up}
 
 var current_state
+var is_player_down
 
 func _ready():
 	current_state = State.Idle
@@ -15,6 +17,8 @@ func _physics_process(delta):
 	player_falling(delta)
 	player_idle(delta)
 	player_run(delta)
+	player_jump()
+	player_down()
 	
 	move_and_slide()
 	
@@ -25,7 +29,7 @@ func player_falling(delta):
 		velocity.y += 1000 * delta
 
 func player_idle(delta):
-	if is_on_floor():
+	if is_on_floor() and velocity.x == 0 and !is_player_down:
 		current_state = State.Idle
 
 func player_run(delta):
@@ -39,9 +43,66 @@ func player_run(delta):
 	if direction != 0:
 		current_state = State.Run
 		animated_sprite_2d.flip_h = false if direction > 0 else true
+		
+func player_jump():
+	if Input.is_action_pressed("jump") and is_on_floor():
+			velocity.y = JUMP_FORCE
+			
+func player_get_down():
+	if Input.is_action_just_pressed("down") and is_on_floor() and !is_player_down:
+			current_state = State.Get_Down
+			is_player_down = true
+	
+func player_stay_down():
+	if Input.is_action_pressed("down") and is_player_down:
+		current_state = State.Stay_Down
+		
+func player_get_up():
+	if Input.is_action_just_released("down") and is_player_down:
+		current_state = State.Get_Up
+		is_player_down = false
+		
+func player_down():
+	if Input.is_action_just_pressed("down") and is_on_floor() and !is_player_down:
+		current_state = State.Get_Down
+		is_player_down = true
+
+	elif Input.is_action_pressed("down") and is_player_down:
+		current_state = State.Stay_Down
+
+	elif Input.is_action_just_released("down") and is_player_down:
+		current_state = State.Get_Up
+		is_player_down = false
+
+func update_state():
+	if Input.is_action_pressed("down") and is_on_floor():
+		player_down()
+	elif !is_on_floor():
+		if velocity.y < 0:
+			current_state = State.Jump
+		else:
+			current_state = State.Fall
+	elif velocity.x != 0:
+		current_state = State.Run
+	else:
+		current_state = State.Idle
 
 func player_animations():
 	if current_state == State.Idle:
 		animated_sprite_2d.play("idle")
 	elif current_state == State.Run:
 		animated_sprite_2d.play("run")
+	elif current_state == State.Jump:
+		animated_sprite_2d.play("jump")
+	elif current_state == State.Fall:
+		animated_sprite_2d.play("fall")
+	elif current_state == State.Get_Down:
+		animated_sprite_2d.play("get_down")
+	elif current_state == State.Stay_Down:
+		animated_sprite_2d.play("stay_down")
+	elif current_state == State.Get_Up:
+		animated_sprite_2d.play("get_up")
+
+func _on_area_entered(area):
+	if area.is_in_group("apple"):
+		area.queue_free()
