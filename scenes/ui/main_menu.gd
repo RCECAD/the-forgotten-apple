45add0e1d422782398c2387143426479627d98f1
dudaclw <leegabi08@gmail.com>
2026-06-10 21:19:32 -0300

    feat: persist player health across scenes

--- scenes/ui/main_menu.gd
+++ scenes/ui/main_menu.gd
@@ -14,6 +14,7 @@ func _ready() -> void:
 	_difficulty_slider.value_changed.connect(_on_difficulty_changed)
 
 func _on_novo_jogo_pressed() -> void:
+	get_node("/root/GameState").call("reset_player_health")
 	get_node("/root/SceneTransition").transition_to("res://scenes/levels/bedroom_level.tscn")
 
 func _on_sair_pressed() -> void:
