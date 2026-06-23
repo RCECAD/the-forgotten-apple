e5bc99ef4e812c4062ec79e246cfe5634d41ccfa
giulliaVilanova <govilanova@gmail.com>
2026-06-23 11:49:35 -0300

    fix: apple collectable only once

--- scenes/items/apple_collectible.gd
+++ scenes/items/apple_collectible.gd
@@ -1,10 +1,28 @@
 extends Area2D
 
+@export var collectible_id := "cabin_apple"
+@export var heal_amount := 1
+
 func _ready() -> void:
+	var game_state := get_node("/root/GameState")
+
+	if game_state.is_collectible_collected(collectible_id):
+		queue_free()
+		return
+
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		if body.has_method("heal"):
-			body.heal(1)
+	if !body.is_in_group("player"):
+		return
+
+	var game_state := get_node("/root/GameState")
+
+	if !game_state.collect_collectible(collectible_id):
 		queue_free()
+		return
+
+	if body.has_method("heal"):
+		body.heal(heal_amount)
+
+	queue_free()
--- scenes/player/game_state.gd
+++ scenes/player/game_state.gd
@@ -6,13 +6,26 @@ var max_health := DEFAULT_MAX_HEALTH
 var health := DEFAULT_MAX_HEALTH
 var intro_dialog_seen := false
 
+var collected_collectibles: Dictionary = {}
+
 func reset_player_health() -> void:
 	max_health = DEFAULT_MAX_HEALTH
 	health = max_health
 
 func reset_narrative_progress() -> void:
 	intro_dialog_seen = false
+	collected_collectibles.clear()
 
 func set_player_health(current_health: int, current_max_health: int) -> void:
 	max_health = maxi(current_max_health, 1)
 	health = clampi(current_health, 0, max_health)
+
+func is_collectible_collected(collectible_id: String) -> bool:
+	return collected_collectibles.has(collectible_id)
+
+func collect_collectible(collectible_id: String) -> bool:
+	if collected_collectibles.has(collectible_id):
+		return false
+
+	collected_collectibles[collectible_id] = true
+	return true
